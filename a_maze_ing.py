1ea0875fb7a3b58572e0d3e03de168edb3b6ae87
Aurelie Nogueira <aurelienog@gmail.com>
2026-05-05 18:22:43 +0200

    added load_maze_config

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 import sys
-from src.validation import ConfigError, Config, get_config
-from src.mazegen import MazeGenerator
+from mazegen.config import Config, ConfigError, get_config
+from mazegen.maze import MazeGenerator, MazeError, validate_maze_config
 
 
 def read_file(file: str) -> list[str]:
     with open(file, "r") as f:
         content = [line.strip() for line in f]
     return content
+
+
+def load_maze_config(content: list[str]) -> Config:
+    config = get_config(content)
+    validate_maze_config(config)
+    return config
 
 
 def main() -> None:
@@ -19,7 +25,7 @@ def main() -> None:
 
     try:
         content: list[str] = read_file(file)
-        config: Config = get_config(content)
+        config = load_maze_config(content)
     except FileNotFoundError:
         print("[ERROR] file not found")
         sys.exit(1)
@@ -30,11 +36,15 @@ def main() -> None:
         print("[ERROR] system error")
         sys.exit(1)
     except ConfigError as e:
-        print(f"\n[CONFIG ERROR] Mandatory configuration is invalid:\n{e}")
+        print(f"\n[CONFIG ERROR] Mandatory configuration is invalid:\n- {e}")
         sys.exit(1)
+    except MazeError as e:
+        print(f"\n[MAZE ERROR]\n- {e}")
+        sys.exit(1)
+
     generator = MazeGenerator()
-    maze = generator.generate_DFS_maze(config["WIDTH"], config["HEIGHT"],
-                                       config["ENTRY"], config["EXIT"])
+    maze = generator.generate_perfect_maze(config["WIDTH"], config["HEIGHT"],
+                                           config["ENTRY"], config["EXIT"])
     maze.solve_maze()
     print(maze)
 
